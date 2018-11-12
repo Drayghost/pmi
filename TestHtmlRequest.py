7d2a01536388b5b47d0b78b9596cf80db9918ba4
Thomas <41819951+Drayghost@users.noreply.github.com>
2018-11-12 20:02:58 +0100

    Add files via upload

---
+++ TestHtmlRequest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 12 16:58:24 2018
+
+@author: Thomas
+"""
+
+import urllib.request
+
+source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
